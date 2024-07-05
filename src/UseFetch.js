5465721a9a2454cc19d2013005ce104fe122d7e1
samsunil1999 <samsunil099@gmail.com>
2024-07-05 10:04:47 +0530

    abort controller in useFetch hook

--- src/UseFetch.js
+++ src/UseFetch.js
@@ -1,18 +1,19 @@
 import { useState, useEffect } from "react";
-// Making custom Hook called useFetch.
-// custom hook in react should start with use... keyword.
-
-// This custom hook it made to call any fetch API and get the data, error (if any) & isPending
-// As this functionality could be used in multiple places.
-
+/*
+Abort Controller:
+ an AbortController is a tool provided by the Fetch API to abort asynchronous operations like fetch requests.
+ However, in React Router, an AbortController can be used to handle navigation cancellation and other async operations associated with routing.
+*/
 const useFetch = (url) => {
     const [data, setData] = useState(null);
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
+        const abortController = new AbortController(); // creating the abort controller
+
         setTimeout(() => {
-            fetch(url)
+            fetch(url, { signal: abortController.signal }) // associating the abort controller with its signal
                 .then(res => {
                     if (!res.ok) { 
                         throw Error('Could not fetch the data');
@@ -33,9 +34,11 @@ const useFetch = (url) => {
                     setIsPending(false);
                 });
         }, 1000);
-    },[ url ]) // here we are adding the url as the dependency bcz we need to execute this useEffect whenever the url is changed
+        // any function returned in the useEffect hook is a cleanup function 
+        // this cleanup function executes everytime we exit from the component
+        return () => abortController.abort() // aborting the fetch using the abort method of abortController
+    },[ url ])
 
-    // As generally a hook would return the data array as the result but here in the useFetch function we are return all the states i.e. the data, error & isPending
     return { data, isPending, error };
 }
 

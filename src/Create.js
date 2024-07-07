a5872891169f7209cfd106a0f840006836d26bd5
samsunil1999 <samsunil099@gmail.com>
2024-07-07 17:31:03 +0530

    post API call and submit button on create form

--- src/Create.js
+++ src/Create.js
@@ -4,10 +4,37 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
+
+    const [isPending, setIsPending] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+
+        setIsPending(true);
+        console.log('is pending true');
+
+        setTimeout(() => { // Here settimeout is just used to delay the fetch API call and we could see the loading message in the button
+            // Fetch API called with the method, headers and body
+            fetch('http://localhost:8000/blogs', {
+                method: 'POST',
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(blog)
+            }).then(() => {
+                console.log('New blog added');
+            })
+            setIsPending(false);
+            console.log('is pending false');
+        },1000)
+        
+        
+        
+    }
+
     return (
         <div className="create">
             <h2>Create a new Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Title:</label>
                 <input
                     type="text"
@@ -29,7 +56,9 @@ const Create = () => {
                     <option value="mario">Mario</option>
                     <option value="yoshi">Yoshi</option>
                 </select>
-                <button>Add Blog</button>
+                {/* A button to submit and add a blog into the data */}
+                {!isPending && <button>Add Blog</button>} 
+                {isPending && <button disabled>Loading...</button>} 
             </form>
         </div>
     );

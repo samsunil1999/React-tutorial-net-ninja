24f34f9e73cb4893bc2ecefde83fd08ec6b60abd
samsunil1999 <samsunil099@gmail.com>
2024-07-07 21:59:01 +0530

    delete blog

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,27 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./UseFetch";
+import { useHistory } from "react-router-dom";
 
 const BlogDetails = () => {
     const { id } = useParams();
 
     // reusing the useFetch custom hook
     const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id)
+
+    const history = useHistory();
     
+
+
+    const handleDelete = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: "DELETE",
+            // headers: {"Content-Type": "application/json"},
+        }).then(() => {
+            console.log('deleted');
+            history.push('/');
+        })
+    }
+
     return (
         <div className="blog-details">
             {/* If isPending is true the show Loading... */}
@@ -20,6 +35,7 @@ const BlogDetails = () => {
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
                     <div>{ blog.body }</div>
+                    <button onClick={handleDelete}>Delete</button>
                 </article>
             )}
         </div>

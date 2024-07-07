54498d371cdc3ca41566d972441377329f91864e
samsunil1999 <samsunil099@gmail.com>
2024-07-07 23:35:36 +0530

    notfound component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   return (
@@ -20,6 +21,11 @@ function App() {
             {/* this is an example of route with a param */}
             <Route path="/blogs/:id">
               <BlogDetails />
+            </Route>
+            {/* Here * means include all path as we declare the notFound path at the end of the 
+            Switch therefore it will allow to route all the paths mentioned above this route */}
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </div>
